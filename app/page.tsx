5ce98cf1dba26ac64b9879fd91313f2249ba5c38
minhlam3118410220 <minhlam3118410220@gmail.com>
2024-10-02 22:30:38 +0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-2xl font-bold tracking-tight">Trang chủ</h2>
           <div className="hidden items-center space-x-2 md:flex">
-            <Button>Download</Button>
+            <Button>Check</Button>
           </div>
         </div>
         
